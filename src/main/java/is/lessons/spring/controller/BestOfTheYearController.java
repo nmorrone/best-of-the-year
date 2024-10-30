e0ec2e4998dd025ce0cf7002bd9a828272cc72cb
nmorrone <n.morrone96@gmail.com>
2024-10-30 19:45:28 +0100

    first-commit

---
+++ src/main/java/is/lessons/spring/controller/BestOfTheYearController.java
@@ -0,0 +1,8 @@
+package is.lessons.spring.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BestOfTheYearController {
+
+}
---
+++ src/test/java/is/lessons/spring/BestOfTheYearApplicationTests.java
@@ -0,0 +1,13 @@
+package is.lessons.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestOfTheYearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
